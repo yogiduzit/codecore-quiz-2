409399cfe3249154e0f1cb4e4e00a1b30b854adf
yogiduzit <yverma2@my.bcit.ca>
2019-07-22 15:05:30 -0700

    Added user association

--- spec/factories/ideas.rb
+++ spec/factories/ideas.rb
@@ -2,5 +2,6 @@ FactoryBot.define do
   factory :idea do
     title {Faker::Hacker.say_something_smart}
     body {Faker::Hipster.paragraph(2, true, 4)}
+    association(:user, factory: :user)
   end
 end
