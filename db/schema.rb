a097e5565076e8d2ee8ccebd22dbb70181524a39
yogiduzit <yverma2@my.bcit.ca>
2019-07-22 13:50:47 -0700

    Modified password column

--- db/schema.rb
+++ db/schema.rb
@@ -27,7 +27,7 @@ ActiveRecord::Schema.define(version: 2019_07_22_193054) do
   create_table "users", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
-    t.string "password"
+    t.string "password_digest"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
