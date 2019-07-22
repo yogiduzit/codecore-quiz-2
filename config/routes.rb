78c4a96ea7d3340fd173378d91e5cee35866ffcc
yogiduzit <yverma2@my.bcit.ca>
2019-07-22 16:23:37 -0700

    Set up the routes for reviews

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :ideas
+  resources :ideas do 
+    resources :reviews, only: [:create, :destroy]
+  end
+  
   resources :users, only: [:new, :create]
   resource :sessions, only: [:new, :create, :destroy]
   get '/', {to: 'ideas#home', as: 'home_page'}
