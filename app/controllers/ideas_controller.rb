91383cc164e81cc4bcc3d22966e9a32297ebe499
yogiduzit <yverma2@my.bcit.ca>
2019-07-22 19:06:55 -0700

    Add user after create

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -22,6 +22,7 @@ class IdeasController < ApplicationController
 
   def create
     @idea = Idea.new idea_params
+    @idea.user = current_user
 
     if @idea.save
       redirect_to idea_path(@idea)
